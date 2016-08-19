4225d01a958c2f9a02045d75b544745981147e77
Lars Kristensen <lmkr@hib.no>
2016-08-19 11:50:05 +0200

    En liten endring mer

--- src/no/hib/dat100/Kalkulator.java
+++ src/no/hib/dat100/Kalkulator.java
@@ -9,13 +9,12 @@ public class Kalkulator {
 	public static int sub(int tall1, int tall2) {
 		return tall1 - tall2;
 	}
-
-	public static int div(int tall1, int tall2) {
-		return tall1 / tall2;
-	}
 	
 	public static int mul(int tall1, int tall2) {
 		return tall1 * tall2;
 	}
 
+	public static int div(int tall1, int tall2) {
+		return tall1 / tall2;
+	}
 }
